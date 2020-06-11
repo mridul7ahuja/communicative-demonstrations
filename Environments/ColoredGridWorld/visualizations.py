450e50c96a8516251951e4b6d901b01009ba6ffa
Mridul Ahuja <58695866+mridul7ahuja@users.noreply.github.com>
2020-06-11 19:18:31 +0530

    Add files via upload

--- Environments/ColoredGridWorld/visualizations.py
+++ Environments/ColoredGridWorld/visualizations.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                AutoMinorLocator)
+from matplotlib.path import Path
+import matplotlib.patches as patches
 
 def viewDictionaryStructure(d, dictionaryType, indent=0):
     if dictionaryType == "t":
@@ -263,38 +265,109 @@ def visualizeTransitionTable(states, transitionTable, actionOfInterest, arrowSca
                 head_length=0.1*actionProb, 
                 color = arrowColor)    
     plt.show()
+            
+"""
+    Visualizes the environment and all the different trajectories given as input
+    Inputs:
+        states: list of state position tuples (x,y)
+        goalStates: list of possible goals 
+        trapStates: list of obstacle or trap spaces
+        trajectories: list of all trajectories to be plotted
+        goalNameDictionary: dictionary where keys are state tuples and values are names of those states. Use if you want to name goals.     
+        gridScale: to change the scale of the environment
+        jitterMean: mean of the normal distribution from which the slight shift in the plotted trajectories are sampled from 
+        jittterVar: variance of the normal distribution from which the slight shift in the plotted trajectories are sampled from 
+"""
+            
+def visualizeEnvironmentMultipleTraj(states, goalStates = [], trapStates = [], trajectories = [], goalNameDictionary = {}, gridScale = 1.5, jitterMean = 0, jitterVar = 0.03):
+    gridAdjust = .5
+
+    minimumx, minimumy = [min(coord) for coord in zip(*states)]
+    maximumx, maximumy = [max(coord) for coord in zip(*states)]
+    
+    plt.rcParams["figure.figsize"] = [(maximumx-minimumx)*gridScale, (maximumy-minimumy)*gridScale]
+    ax = plt.gca(frameon=False, xticks = range(minimumx-1, maximumx+2), yticks = range(minimumy-1, maximumy+2))
+    start, end = ax.get_xlim()
+    ax.xaxis.set_ticks(np.arange(0, end, 1.0))
+    start, end = ax.get_ylim()
+    ax.yaxis.set_ticks(np.arange(0, end, 1.0))
+
+    #gridline drawing
+    for (statex, statey) in states:
+        ax.add_patch(Rectangle((statex-gridAdjust, statey-gridAdjust), 1, 1, fill=False, color='black', alpha=1))
+
+    #goal coloring and labeling
+    for (goalx,goaly) in goalStates:
+        ax.add_patch(Rectangle((goalx-gridAdjust, goaly-gridAdjust), 1, 1, fill=True, color='green', alpha=.1))
+        if (goalx, goaly) in goalNameDictionary.keys():
+            ax.text(goalx-.15, goaly-.15, goalNameDictionary[(goalx, goaly)], fontsize = 12)
+    
+    for (x,y),colour in states.items():
+        ax.add_patch(Rectangle((x-gridAdjust, y-gridAdjust), 1, 1, fill=True, color=colour, alpha=.1))
+
+    for (trapx, trapy) in trapStates:
+        ax.add_patch(Rectangle((trapx-gridAdjust, trapy-gridAdjust), 1, 1, fill=True, color='red', alpha=.1))
+
+    #trajectory path drawing for multiple trajectories
+    for trajectory in trajectories:
+        jitter = np.random.normal(jitterMean, jitterVar, 2)
+        verts = []
+        for indx, (statex, statey) in enumerate(trajectory):
+            if( indx!=0 and indx!=(len(trajectory)-1) ):
+                verts.append((statex + jitter[0], statey + jitter[1]))
+            else:
+                verts.append((statex, statey))
+        codes = [Path.MOVETO]
+        for i in range(len(verts)-1):
+            codes.append(Path.LINETO)
+        path = Path(verts, codes)
+        patch = patches.PathPatch(path, facecolor='None', lw=0.5, capstyle = 'butt')
+        for indx, (statex, statey) in enumerate(trajectory):
+            ax.add_patch(Rectangle((statex-gridAdjust, statey-gridAdjust), 1, 1, fill=False, alpha=.1))
+            ax.add_patch(patch)
+
+    plt.show()
     
 """
-    Visualizes the belief states of the trajectory by plotting the pmf over the possible colour rewards at each step. 
+    Graph for probability at each time step for different objects in the given dictionary
+    Input:
+        probAtEachStepDict: dictionary of _: list of probability at each time step
+        title, xlabel: Title for X-axis, ylabel: Title for Y-axis
+"""
+    
+def plotProbAtEachStep(probAtEachStepDict, title="", xlabel = "Time Point", ylabel = "Posterior Probability"):
+    X = [i for i in range(1,len(list(probAtEachStepDict.values())[0])+1)]
+    for env in probAtEachStepDict.keys():
+        Y = probAtEachStepDict[env]
+        plt.plot(X,Y, label = env, linestyle='dashed',marker='o', markersize=4)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.legend()
+    plt.show()
+"""
+    Visualizes the belief states of the trajectory by plotting the pmf over the possible colour rewards at each step in the form a bar chart
     Inputs: 
-        jointTraj: list of (objectState, beliefState) tuples over the trajectory
-        orderOfTrapColours: ordered list of possible trap colours to set the x-axis labels corresponding to each colour reward
-        figSize = tuple for the figure size of the entire plot
+        dictAtEachStep: list of beliefStates(dictionaries) over the trajectory at each step 
+        figSize: tuple for the figure size of the entire plot
+        showHeight: boolean for showing the heights of each bar or not
 """
-    
-def beliefStateInTrajectoryGraph(jointTraj, orderOfTrapColours, figSize = (10,20)):
-    rows = len(jointTraj)
+   
+def plotDictAtEachStep(dictAtEachStep, figSize = (10,20), majorYTick = 0.5, minorYTick = 0.25, showHeight = False):
+    rows = len(dictAtEachStep)
     fig, ax = plt.subplots(rows, 1, sharex = 'col', sharey = 'col', figsize= figSize)
-    for i in range(len(jointTraj)):
-        D = jointTraj[len(jointTraj)-1-i][1]()
-        envLabels = {}
-        for env in D.keys():
-            colourReward = env[0]()
-            label = ""
-            for colour in orderOfTrapColours:
-                if(colourReward[colour]<0):
-                    label = label + "x"
-                else: 
-                    label = label + "o"
-            envLabels[env] = label
-        D = {envLabels[env]:prob for env, prob in D.items()}    
+    for i in range(len(dictAtEachStep)):
+        D = dictAtEachStep[len(dictAtEachStep)-1-i]
         lists = sorted(D.items()) 
         x, y = zip(*lists) 
         ax[i].bar(range(len(D)), list(y), align='center', tick_label = list(x))
-        ax[i].yaxis.set_major_locator(MultipleLocator(0.5))
-        ax[i].yaxis.set_minor_locator(MultipleLocator(0.25))
-        if(i==(len(jointTraj) -1)):
-            ax[i].set(xlabel='Reward Function', ylabel="Step " + str(len(jointTraj) - i-1))
+        ax[i].yaxis.set_major_locator(MultipleLocator(majorYTick))
+        ax[i].yaxis.set_minor_locator(MultipleLocator(minorYTick))
+        if(showHeight):
+            for p in ax[i].patches:
+                ax[i].annotate(str(round(p.get_height(), 3)), (p.get_x() * 1.005, p.get_height() * 1.05))
+        if(i==(len(dictAtEachStep) -1)):
+            ax[i].set(xlabel='Reward Function', ylabel="Step " + str(len(dictAtEachStep) - i-1))
         else:
-            ax[i].set(ylabel="Step " + str(len(jointTraj) - i-1))
+            ax[i].set(ylabel="Step " + str(len(dictAtEachStep) - i-1)) 
         
